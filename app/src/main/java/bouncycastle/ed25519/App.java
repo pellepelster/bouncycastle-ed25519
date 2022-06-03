f7b80fe11a7a721cc48804e0a0af9a7487a03389
Christian 'Pelle' Pelster <pelle@pelle.io>
2022-06-03 23:02:42 +0200

    add failing example

---
+++ app/src/main/java/bouncycastle/ed25519/App.java
@@ -0,0 +1,32 @@
+package bouncycastle.ed25519;
+
+import org.bouncycastle.jcajce.spec.EdDSAParameterSpec;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.openssl.jcajce.JcaPEMWriter;
+import org.bouncycastle.util.io.pem.PemObject;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.security.*;
+
+public class App {
+    public static void main(String[] args) throws NoSuchAlgorithmException, NoSuchProviderException, InvalidAlgorithmParameterException, IOException {
+
+        Security.addProvider(new BouncyCastleProvider());
+
+        var keyPairGenerator = KeyPairGenerator.getInstance("Ed25519", "BC");
+        keyPairGenerator.initialize(new EdDSAParameterSpec(EdDSAParameterSpec.Ed25519), new SecureRandom());
+
+        var keyPair = keyPairGenerator.generateKeyPair();
+
+        var privateKey = new PemObject("EC PRIVATE KEY", keyPair.getPrivate().getEncoded());
+
+        try (var bufferedWriter = new BufferedWriter(new FileWriter("key.pem"))) {
+            try (var pemWriter = new JcaPEMWriter(bufferedWriter)) {
+                pemWriter.writeObject(privateKey);
+            }
+        }
+    }
+}
